5440949ba566b09a8771934f1993097c7321e43d
Obi <obinnawalker@gmail.com>
2017-11-10 14:10:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,21 +6,24 @@ class BoardMember{
   }
 
   veto(){
-    return "No, I must disagree"    
+    return "No, I must disagree"
   }
   approve(){
-    return "You can do that!"    
+    return "You can do that!"
   }
   doCharity(){
-    return "I like to help people."    
+    return "I like to help people."
   }
   releasePressStatement(){
     return "You will see great things from Scuber."
-    
+
   }
   sayHi(){
     return `Hi, my name is ${name}. I am from ${homeState}, and I was trained in ${training}`
   }
+}
 
+class Ceo extends BoardMember{
+  
   
 }
